2a9a1d2d0e0c54c0ad1004797267abb949e063cf
Dan Edmonds <dan@danedmonds.net>
2013-03-28 10:19:28 -0400

    Initial site commit

---
+++ index.php
@@ -0,0 +1,233 @@
+<?php
+require_once('mobile_device_detect.php');
+mobile_device_detect(true,true,true,true,true,true,true,'http://m.danedmonds.net',false); 
+?>
+
+<!DOCTYPE html>
+
+<head>
+	
+	<meta charset="UTF-8">
+
+	<title>Dan Edmonds &raquo; freelance web designer &amp; WordPress developer &mdash; Greensburg, PA</title>
+    
+    <meta name="description" content="I'm Dan Edmonds, and I'm a freelance user interface designer &amp; web developer from Greensburg, PA. I specialize in user interface design and WordPress development." />
+    <meta name="keywords" content="dan edmonds, pittsburgh designer, pittsburgh developer, pittsburgh web design, pittsburgh web developer, greensburg web design, greensburg web developer, greensburg designer, greensburg developer, irwin designer, irwin developer, irwin web designer, irwin web developer" />
+	<meta name="author" content="Dan Edmonds" />
+	
+	<link rel="stylesheet" href="style.css" media="screen, projection" />
+    
+	<script src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+    <script src="scripts.js"></script>
+    
+    <script type="text/javascript" src="http://use.typekit.com/quw8pre.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    
+    <!-- Place this tag in your head or just before your close body tag -->
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+    
+	<script type="text/javascript">
+    
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-27923426-1']);
+      _gaq.push(['_trackPageview']);
+    
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+    
+    </script>
+	
+</head>
+
+<body>
+
+    <ul id="share">
+    
+        <li>
+        	<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="danEdmonds">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+		</li>
+        
+        <li>
+        	<!-- Place this tag where you want the +1 button to render -->
+        	<g:plusone size="tall"></g:plusone>
+        </li>
+        
+        <li>
+			<!--<script src="http://platform.linkedin.com/in.js" type="text/javascript"></script>
+            <script type="IN/Share" data-url="http://danedmonds.net" data-counter="top"></script>-->
+        </li>
+    
+    </ul>
+
+	<!-- wrapper -->
+	<div id="wrapper">
+    
+        <!-- wrapper-inner -->
+        <div id="wrapper-inner">
+            
+            <!-- header -->
+            <div id="header">
+                
+                <div id="logo"><a href="http://danedmonds.net/" title="Logo Link"><span>&nbsp;</span></a></div>
+                
+                <div id="about-button"><a href="#about" title="My Story"><span>&nbsp;</span></a></div>
+                
+          	</div>
+            <!-- /header -->
+            
+            <!-- work -->
+            <div id="work">
+            
+            	<h1 id="heading-work">my work.</h1>
+            
+                <div class="project">
+                    
+                    <div class="project-image">
+                        <a href="http://wvsokids.org/" title="West Virginia Symphony Orchestra Kids" target="_blank"><img src="images/wvsokids.jpg" width="420" height="250" alt="" /></a>
+                    </div>
+                    
+                    <h3>WVSO Kids</h3>
+                    
+                    <p><strong>Role:</strong> Site Design, Branding, XHTML/CSS, WordPress, jQuery, Flash &amp; AS3</p>
+                
+                </div>
+                
+                <div class="project">
+                    
+   				  <div class="project-image">
+                        <a href="archives/naturalwellnesscenter/" title="Natural Wellness Center" target="_blank"><img src="images/naturalwellnesscenter.jpg" width="420" height="250" alt="" /></a>
+                    </div>
+                    
+                    <h3>Natural Wellness Center &mdash; Concept</h3>
+                    
+                    <p><strong>Role:</strong> Client Collaboration, Site Design, XHTML/CSS</p>
+                
+                </div>
+                
+                <div class="launch-site-wrap">
+                
+   				  <div class="launch-site left">
+                    
+                    	<div class="launch-site-button"><a href="http://wvsokids.org/" title="WVSO Kids" target="_blank"><span>&nbsp;</span></a></div>
+                    
+                    </div>
+                    
+                    <div class="launch-site right">
+                    
+                    	<div class="launch-site-button"><a href="archives/naturalwellnesscenter/" title="Natural Wellness Center" target="_blank"><span>&nbsp;</span></a></div>
+                    
+                    </div>
+                
+                </div>
+                
+                <div class="project" style="clear: both">
+                  
+                  <div class="project-image">  
+                      <a href="archives/richdataland" title="Richard W. Land" target="_blank"><img src="images/richdataland.jpg" width="420" height="250" alt="" /></a>
+                  </div>
+                  
+                  <h3>Richard W. Land</h3>
+                    
+                  <p><strong>Role:</strong> Client Collaboration, Site Design, XHTML/CSS, jQuery</p>
+                
+                </div>
+                
+                <div class="launch-site-wrap">
+                
+   				  <div class="launch-site left">
+                    
+                    	<div class="launch-site-button"><a href="http://richdataland.net/" title="Richard W. Land" target="_blank"><span>&nbsp;</span></a></div>
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+            <!-- /work -->
+            
+            <!-- about -->
+            <div id="about">
+            
+            	<div id="up-button"><a href="#header" title="Go to top"><span>&nbsp;</span></a></div>
+            
+                <div id="inner">
+                
+                    <div>
+                    
+                        <h1 id="heading-about">hi, i'm dan edmonds.</h1>
+                        
+                        <div id="bio">
+                        
+                        	<p>I'm a passionate user interface designer and WordPress developer based just outside of Pittsburgh, Pennsylvania working as the part-time Multimedia Producer at <a href="http://www.pmi.tv" target="_blank">PMI</a>.</p>
+                            
+                            <p>I love crafting beautiful and intuitive design solutions with an emphasis on simplicity, usability and accessibility. My admiration for web standards is perfectly complemented by executing clean, hand-coded mark-up while keeping readability and clarity of content in mind.</p>
+                            
+							<p>I’ve recently taken a particular interest in the transition of web content to a mobile format and the design process and development behind it.</p>
+                            
+                            <p>When I'm not working, you can find me with my <!--beautiful--> girlfriend, playing iPhone games, or driving my <a href="http://www.flickr.com/photos/64959750@N02/sets/72157627022788769/" target="_blank">little slice of Europe</a>.</p>
+                            
+                            <h2>email.</h2>
+                            <p><a href="mailto:dan@danedmonds.net">dan@danedmonds.net</a><br />
+                            <!-- <a href="">@danEdmonds</a> <strong>/ twitter</strong><br /> --></p>
+                            
+                            <h2>voice.</h2>
+                            <p>+1 724.493.5919</p>
+                        
+                        </div>
+                        
+                        <div id="contact">
+                            
+                            <h2>capabilities.</h2>
+                            
+                            <ul>
+                                <li>Standards-compliant, <mark>HTML5</mark></li>        
+                                <li>Well-documented, well-organized <mark>CSS3</mark></li>
+                                <li>Experience with <mark>JavaScript, jQuery</mark></li> 
+                                <li>Implementation of <mark>ActionScript 3.0</mark> &amp; XML</li>       
+                                <li>Understanding of <mark>usability testing</mark> solutions</li>
+                                <li><mark>Cross-browser compatibility</mark>, problem-solving</li>
+                                <li>SEO and <mark>accessibility</mark> fundamentals</li>        
+                                <li><mark>Wordpress</mark> theme development</li>
+                                <li>Information architecture, <mark>content strategy</mark></li>
+                                <li>Grids, layout, typography, color theory</li>        
+                                <li>Image editing and graphic design</li>
+                                <li><mark>Video editing</mark> and live streaming video</li>
+                                <li>Project management &amp; <mark>client collaboration</mark></li>
+                            </ul>
+                        
+                        </div>
+                    
+                    </div>
+                    
+                    <div class="clear"></div>                    
+                    <span id="stretch"></span>
+                    
+                </div>
+            
+            </div>            
+            <!-- /about -->
+            
+            <!-- footer -->
+			<div id="footer">
+                
+                <p>&copy; Copyright 2011 Dan Edmonds, All Rights Reserved</p>
+                
+                <!--Featured in the National <A href="http://www.webdesigners-directory.com">Web Design</A> Directory-->
+                
+                <div class="clear"></div>
+                
+          	</div>
+            <!-- /footer -->
+            
+        </div>
+        <!-- /wrapper-inner -->
+    
+    </div>
+	<!-- /wrapper -->
+
+</body>
+
+</html>
