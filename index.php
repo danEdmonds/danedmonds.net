bbc16e73a273957753f963c7925c7d4d45fec8c0
Dan Edmonds <dan@danedmonds.net>
2013-03-28 11:47:28 -0400

    Made index.php look prettier.

--- index.php
+++ index.php
@@ -17,17 +17,17 @@
     
 	<script type="text/javascript">
     
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-27923426-1']);
-      _gaq.push(['_trackPageview']);
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-27923426-1']);
+	_gaq.push(['_trackPageview']);
     
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
     
-    </script>
+	</script>
 	
 </head>
 
@@ -46,7 +46,7 @@
                 
                 <div id="logo"><a href="http://danedmonds.net/" title="Logo Link"><span>&nbsp;</span></a></div>
                 
-          	</header>
+			</header>
             <!-- /header -->
             
             <!-- work -->
@@ -157,7 +157,7 @@
                             <h3>email.</h3>
                             <p><a href="mailto:dan@danedmonds.net">dan@danedmonds.net</a></p>
                         
-                        </div>
+						</div>
                         
                         <div id="contact" class="right">
                             
@@ -179,7 +179,7 @@
                                 <li>Project management &amp; <mark>client collaboration</mark></li>
                             </ul>
                         
-                        </div>
+						</div>
                         
                         <div class="clear"></div> 
                     
@@ -217,7 +217,7 @@
 	<!-- /wrapper -->
 	
 	<script src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
-    <script src="scripts.js"></script>
+	<script src="scripts.js"></script>
 
 </body>
 
