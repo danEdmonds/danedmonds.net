1804b972151ac3dd0e1d270688ebae7829ff903c
Dan Edmonds <dan@danedmonds.net>
2013-04-03 10:45:31 -0400

    Update stylesheet and images

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 
 	<title>Dan Edmonds &raquo; freelance web designer &amp; WordPress developer &mdash; Greensburg, PA</title>
     
-	<meta name="description" content="I'm Dan Edmonds, and I'm a freelance user interface designer &amp; web developer from Greensburg, PA. I specialize in user interface design and WordPress development." />
-	<meta name="keywords" content="dan edmonds, pittsburgh designer, pittsburgh developer, pittsburgh web design, pittsburgh web developer, greensburg web design, greensburg web developer, greensburg designer, greensburg developer, irwin designer, irwin developer, irwin web designer, irwin web developer" />
+    <meta name="description" content="I'm Dan Edmonds, and I'm a freelance user interface designer &amp; web developer from Greensburg, PA. I specialize in user interface design and WordPress development." />
+    <meta name="keywords" content="dan edmonds, pittsburgh designer, pittsburgh developer, pittsburgh web design, pittsburgh web developer, greensburg web design, greensburg web developer, greensburg designer, greensburg developer, irwin designer, irwin developer, irwin web designer, irwin web developer" />
 	<meta name="author" content="Dan Edmonds" />
 	
 	<!-- Check device width for responsive media queries -->
@@ -17,17 +17,17 @@
     
 	<script type="text/javascript">
     
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-27923426-1']);
-	_gaq.push(['_trackPageview']);
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-27923426-1']);
+      _gaq.push(['_trackPageview']);
     
-	(function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
     
-	</script>
+    </script>
 	
 </head>
 
@@ -46,7 +46,7 @@
                 
                 <div id="logo"><a href="http://danedmonds.net/" title="Logo Link"><span>&nbsp;</span></a></div>
                 
-			</header>
+          	</header>
             <!-- /header -->
             
             <!-- work -->
@@ -128,6 +128,18 @@
                 
                 </article>
                 
+                <!--<article class="project">
+                    
+                    <div class="project-image">
+                        <a href="http://www.danedmonds.net/archives/naturalwellnesscenter/" title="Pace School" target="_blank"><img src="images/naturalwellnesscenter.jpg" width="420" height="250" alt="" /></a>
+                    </div>
+                    
+                    <h4>Natural Wellness Center Concept</h4>
+                    
+                    <p><strong>Role:</strong> Site Design, XHTML/CSS</p>
+                
+                </article>-->
+                
                 <div class="clear"></div>
             
             </section>
@@ -157,7 +169,7 @@
                             <h3>email.</h3>
                             <p><a href="mailto:dan@danedmonds.net">dan@danedmonds.net</a></p>
                         
-						</div>
+                        </div>
                         
                         <div id="contact" class="right">
                             
@@ -179,7 +191,7 @@
                                 <li>Project management &amp; <mark>client collaboration</mark></li>
                             </ul>
                         
-						</div>
+                        </div>
                         
                         <div class="clear"></div> 
                     
@@ -217,7 +229,7 @@
 	<!-- /wrapper -->
 	
 	<script src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
-	<script src="scripts.js"></script>
+    <script src="scripts.js"></script>
 
 </body>
 
